9fce2c4bcf6792f2e2607902c0e480fc618e43e4
Sr1jay <164362301+Sr1jay@users.noreply.github.com>
2026-03-27 23:33:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -68,4 +68,5 @@ app.post("/render", upload.single("audio"), async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server running on 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on", PORT));
